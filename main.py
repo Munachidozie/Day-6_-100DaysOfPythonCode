809cbc7b64793e8dfbaac6af293bc3239e67612c
Munachimso Ukaoha <65025073+Munachidozie@users.noreply.github.com>
2023-01-17 14:37:49 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+print ("-- My Login System --")
+print ("")
+
+username = input ("Input your username: ")
+password = input ("Input your password: ")
+
+if username == "chris" and password == "chr15":
+  print ("Welcome back Mark. I could spot that bald head of yours from afar.")
+elif username == "tofunmi" and password == "vawulence":
+  print ("Welcome Back Tofunmi. You could had charmed your way in here with your beauty.")
+elif username == "Dapo" and password == "iwologbonju":
+  print ("Welcome Dapo. ALways nice to see you although we hoped you would have kept your oversbi self away for a while")
+else:
+  print ("Sorry but you're not allowed here. My creator only had time to add 3 people who were closeby when he wrote this but spent over an hour coming up with not-so funny comments to the correct users. IKR 😒")
